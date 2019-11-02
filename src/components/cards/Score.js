c0c4bfe23408aa74b54de1566042541877a13cc2
pedrorijo91 <pedrorijo91@gmail.com>
2019-11-02 13:16:45 +0000

    Implement score function and UT

--- src/components/cards/Score.js
+++ src/components/cards/Score.js
@@ -1,8 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const handScore = cards => {
-  return 10; // FIXME and add UT
+const cardScore = card => {
+  if (["J", "Q", "K"].includes(card.rank)) {
+    return 10;
+  }
+
+  if (card.rank === "A") {
+    return 1;
+  }
+
+  return parseInt(card.rank);
+};
+
+export const handScore = cards => {
+  const nonAcesScore = cards
+    .map(card => cardScore(card))
+    .reduce((a, b) => a + b, 0);
+
+  const aces = cards.filter(card => card.rank === "A").length;
+
+  if (aces > 0 && nonAcesScore <= 11) {
+    return nonAcesScore + 10; // we promote one A to 11
+  } else {
+    return nonAcesScore;
+  }
 };
 
 const Score = ({ cards }) => <span>Score: {handScore(cards)}</span>;
---
+++ src/components/cards/specs/Score.spec.js
@@ -0,0 +1,143 @@
+import { handScore } from "../Score";
+import { CARD_SUITS } from "../Card";
+
+describe("hand score", () => {
+  it("should be 0 for empty hand", () => {
+    expect(handScore([])).toEqual(0);
+  });
+
+  it("should be the value of the number in single card hand", () => {
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "10"
+        }
+      ])
+    ).toEqual(10);
+
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "2"
+        }
+      ])
+    ).toEqual(2);
+
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "7"
+        }
+      ])
+    ).toEqual(7);
+  });
+
+  it("should be the figure value in single card hand", () => {
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "Q"
+        }
+      ])
+    ).toEqual(10);
+
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "K"
+        }
+      ])
+    ).toEqual(10);
+
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "J"
+        }
+      ])
+    ).toEqual(10);
+  });
+
+  it("should be the sum of the card values", () => {
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "2"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "3"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "Q"
+        }
+      ])
+    ).toEqual(15);
+  });
+
+  it("should be using A as 11 if it doesn't bust hand", () => {
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "2"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "3"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "A"
+        }
+      ])
+    ).toEqual(16);
+  });
+
+  it("should be using A as 1 if it busts hand", () => {
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "10"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "3"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "A"
+        }
+      ])
+    ).toEqual(14);
+  });
+
+  it("should be using A with different values if needed", () => {
+    expect(
+      handScore([
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "2"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "A"
+        },
+        {
+          suit: CARD_SUITS.SPADES,
+          rank: "A"
+        }
+      ])
+    ).toEqual(14);
+  });
+
+});
