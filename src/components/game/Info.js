bac6519d0a1d4a674fd084e05725859ba6addb99
pedrorijo91 <pedrorijo91@gmail.com>
2019-11-13 21:58:33 +0000

    Fix Info messages

--- src/components/game/Info.js
+++ src/components/game/Info.js
@@ -1,18 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Info = ({waitingForBet, playerWon, dealerWon}) => (
+const Info = ({waitingForBet, winner}) => (
   <div style={{fontWeight: "bold"}}>
     {waitingForBet && "Please place your bet and click 'Start Game' to start the round" }
-    {dealerWon && "Game over. Click 'Start Game' to start the round" }
-    {playerWon && "You won. Click 'Start Game' to start the round" }
+    {winner === 'DEALER' && "Game over. Click 'Start Game' to start the round" }
+    {winner === 'PLAYER' && "You won. Click 'Start Game' to start the round" }
+    {winner === 'TIE' && "It's a tie! Click 'Start Game' to start the round" }
   </div>
 );
 
 Info.propTypes = {
   waitingForBet: PropTypes.bool.isRequired,
-  playerWon: PropTypes.bool.isRequired,
-  dealerWon: PropTypes.bool.isRequired,
+  winner: PropTypes.string
 };
 
 export default Info;
--- src/containers/InfoContainer.js
+++ src/containers/InfoContainer.js
@@ -2,7 +2,8 @@ import { connect } from "react-redux";
 import Info from "../components/game/Info";
 
 const mapStateToProps = state => ({
-  waitingForBet: state.game.playerHand.length === 0
+  waitingForBet: state.game.playerHand.length === 0,
+  winner: undefined,
 });
 
 export default connect(mapStateToProps)(Info);
